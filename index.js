0e946e9ed73e64bfba2253c8db0492088b256d55
vigvx <pedrovigards0409@gmail.com>
2025-11-26 20:56:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+index
+if ("serviceWorker" in navigator) {
+    console.log("Service Worker is supported");
+navigator.serviceWorker
+.then(objtQueRepORegistro =>
+    console.log(objtQueRepORegistro)
+);
+    
+}else {
+   alert("Service Worker is not supported");
+}
